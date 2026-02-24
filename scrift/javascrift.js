7efe00312afbe7b13f9ddd27906443523fc8d2ac
mdsahidulc243223 <mdr073621@gmail.com>
2026-02-24 09:25:13 +0600

    interview,rejected button count increase and card add

--- scrift/javascrift.js
+++ scrift/javascrift.js
@@ -36,9 +36,12 @@ function toggleStyle(id){
      allcardCount.classList.remove('hidden')
     filteredSection.classList.add('hidden');
  }
+ else if(id==="Reject"){
+     allcardCount.classList.add('hidden')
+    filteredSection.classList.remove('hidden');
+ }
 }
 document.addEventListener('click',function(event){
-    console.log(event.target.classList.contains('btn-1'));
 if(event.target.classList.contains('btn-1')){    
 const parentnode=event.target.parentNode.parentNode;
 const companyName= parentnode.querySelector('.company').innerText;
@@ -63,24 +66,75 @@ const companyExist=interviewList.find(item=> item.companyName===cardInfo.company
 if(!companyExist){
    interviewList.push(cardInfo); 
 }
- renderTriving();}
+interviewList=interviewList.filter(item=> item.companyName !==cardInfo.companyName);
+calculateCount();
+else if(event.target.classList.contains('btn-2')){    
+const parentnode=event.target.parentNode.parentNode;
+const companyName= parentnode.querySelector('.company').innerText;
+const position= parentnode.querySelector('.position').innerText;
+const salary= parentnode.querySelector('.salary').innerText;
+const type= parentnode.querySelector('.type').innerText;
+const description= parentnode.querySelector('.description').innerText;
+const interview= parentnode.querySelector('.btn-1').innerText;
+const rejected= parentnode.querySelector('.btn-2').innerText;
+parentnode.querySelector('.type').innerText="Rejected";
+const cardInfo={
+   companyName,
+   position,
+   salary,
+   type:'Rejected',
+   description,
+   interview,
+   rejected 
+}
+const companyExist=rejectList.find(item=> item.companyName===cardInfo.companyName);
+
+if(!companyExist){
+   rejectList.push(cardInfo); 
+}
+rejectList=rejectList.filter(item=> item.companyName !==cardInfo.companyName);
+calculateCount();
+ renderrejected();}
 });
 function renderTriving(){
 filteredSection.innerHTML=''
 for(let thrive of interviewList){
-    console.log(thrive);
     let div=document.createElement('div')
     div.className='my-10 border bg-white  rounded-[6px] flex justify-between gap-8 w-[1110px] mx-auto'
     div.innerHTML=`
     <div class="p-4 space-y-4">
-        <h2 class=" company font-bold text-xl">${Interview.company}</h2>
-        <p class="position">React Native Developer</p>
-        <p class="salary">Remote • Full-time • $130,000 - $175,000</p>
-        <button class=" type btn">${</button>
-        <P class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</P>
+        <h2 class=" company font-bold text-xl">${thrive.companyName}</h2>
+        <p class="position">${thrive.position}</p>
+        <p class="salary">${thrive.salary}</p>
+        <button class=" type btn">${thrive.type}</button>
+        <P class="description">${thrive.description}</P>
         <diV>
-            <button id="mobile-first-corp-btn-interview" class="btn-1 bg-white text-green-400">Interview</button>
-            <button id="mobile-first-corp-btn-rejected" class="btn-2 bg-white text-red-400">Reject</button>
+            <button id="mobile-first-corp-btn-interview" class=" btn btn-1 bg-white text-green-400">Interview</button>
+            <button id="mobile-first-corp-btn-rejected" class=" btn btn-2 bg-white text-red-400">Reject</button>
+        </diV>
+    </div>
+    <div>
+         <img src="./delete-bin.svg" class="w-5 mt-4 mx-4 text-right">
+    </div>
+    `
+    filteredSection.appendChild(div);
+}
+}
+function renderrejected(){
+filteredSection.innerHTML=''
+for(let reject of rejectList){
+    let div=document.createElement('div')
+    div.className='my-10 border bg-white  rounded-[6px] flex justify-between gap-8 w-[1110px] mx-auto'
+    div.innerHTML=`
+    <div class="p-4 space-y-4">
+        <h2 class=" company font-bold text-xl">${reject.companyName}</h2>
+        <p class="position">${reject.position}</p>
+        <p class="salary">${reject.salary}</p>
+        <button class=" type btn">${reject.type}</button>
+        <P class="description">${reject.description}</P>
+        <diV>
+            <button id="mobile-first-corp-btn-interview" class=" btn btn-1 bg-white text-green-400">Interview</button>
+            <button id="mobile-first-corp-btn-rejected" class=" btn btn-2 bg-white text-red-400">Reject</button>
         </diV>
     </div>
     <div>
